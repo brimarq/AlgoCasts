d4a4494647c283bc7de1407bab256bb90508ed8a
Brian Marquis <rbmarquis.dev@gmail.com>
2021-08-09 22:48:00 -0500

    solve chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  return array.reduce((newArr, el, i, arr) => {
+    if (i % size === 0) {
+      let arrChunk = arr.slice(i, i + size);
+      newArr.push(arrChunk);
+    }
+    return newArr;
+  }, []);
+}
+
+function chunk2(array, size) {
+  const newArr = [];
+  let chunkArr = [];
+
+  while (typeof (i = array.shift()) !== 'undefined') {
+    chunkArr.push(i);
+    if (chunkArr.length === size) {
+      newArr.push(chunkArr);
+      chunkArr = [];
+    }
+  }
+
+  if (chunkArr.length) {
+    newArr.push(chunkArr);
+  }
+
+  return newArr;
+}
 
 module.exports = chunk;
