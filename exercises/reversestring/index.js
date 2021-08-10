cb3c86130a8f9e8320f89eae9c2c317ccbec937b
Brian Marquis <rbmarquis.dev@gmail.com>
2021-08-09 22:40:24 -0500

    solve reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+function reverse2(str) {
+  let rev = '';
+  for (char of str) {
+    rev = char + rev;
+  }
+  return rev;
+}
+
+function reverse3(str) {
+  return str.split('').reduce((rev, char) => character + reversed, '');
+}
+
+// avoid using the older-style for loop
+function reverse4(str) {
+  let rev = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev.push(str[i]);
+  }
+  return rev.join('');
+}
 
 module.exports = reverse;
