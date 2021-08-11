1624510fc49b4ba6d2e85f176a448e37f46c475c
Brian Marquis <rbmarquis.dev@gmail.com>
2021-08-11 01:09:12 -0500

    solve fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+// O2^n exponential complexity, recursive
+function fib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+// Memoize the fib fn
+fib = memoize(fib);
+
+// O(n) linear complexity
+// function fib(n) {
+//   const result = [0, 1];
+//   while (result.length <= n) {
+//     result.push(result[result.length - 2] + result[result.length - 1]);
+//   }
+//   return result[n];
+// }
+
+// O(n) linear complexity
+// function fib(n) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
 
 module.exports = fib;
